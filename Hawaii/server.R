6fc326adf9b01adba4b92d2821195b113ad92dda
dustin-duncan <dustinduncan@ucsb.edu>
2024-02-27 13:46:14 -0800

    Check out my sick maps: Dustin 02/27/24

--- Hawaii/server.R
+++ Hawaii/server.R
@@ -41,22 +41,36 @@ function(input, output, session) {
       filter_selection <- input$data
 
       subset_ahupuaa <- data_sf_clean[data_sf_clean$mokupuni == filter_selection, ]
-      subset_vec <- vect(subset_ahupuaa)
-      return(subset_vec)
+
+      return(subset_ahupuaa)
     })
 
     reactive_map2 <- reactive({
+      filter_selection <- input$data
 
-      multi_layer_reactive_rs <- crop(multi_layer_rs, extent(reactive_map()))
+      subset_ahupuaa <- data_sf_clean[data_sf_clean$mokupuni == filter_selection, ]
+
+      multi_layer_reactive_rs <- crop(multi_layer_rs, extent(subset_ahupuaa))
       multi_layer_reactive_df <- as.data.frame(multi_layer_reactive_rs, xy = TRUE)
 
       return(multi_layer_reactive_df)
     })
 
+    reactive_map3 <- reactive({
+      filter_selection <- input$data
+
+      subset_ahupuaa <- data_sf_clean[data_sf_clean$mokupuni == filter_selection, ]
+      vect(subset_ahupuaa)
+      landuse_reactive_sf <- st_crop(landuse_sf, subset_ahupuaa)
+
+      return(landuse_reactive_sf)
+    })
+
     output$dustin_plot <- renderPlot({
       ggplot() +
-        geom_sf(data = reactive_map()) +
-        geom_tile(data = reactive_map2(), inherit.aes = FALSE, aes(x = x, y = y), fill = "#882222")
+        geom_sf(data = reactive_map3(), aes(fill = resample)) +
+        geom_sf(data = reactive_map(), fill = NA, color = "black") +
+        geom_tile(data = reactive_map2(), inherit.aes = FALSE, aes(x = x, y = y)) +
         theme_bw()
     })
 
