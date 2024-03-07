7ede1423a802d9b18bc3c3730940d83ef860c9e6
akirk6016 <akirk@ucsb.edu>
2024-03-07 11:11:54 -0800

    ak work 3/7

--- Hawaii/global.R
+++ Hawaii/global.R
@@ -291,3 +291,437 @@ area_landuse_sum_dustin <- intersected_polygons_dustin %>%
 
 
 ############################## ABIGAIL WIDGET WORK ###############################
+intrvw_1 <- pdftools::pdf_text(here::here('data','Pioneer Mill Interviews', 'pioneermill_03.pdf'))
+intrvw_2 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_04.pdf'))
+intrvw_3 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews', 'pioneermill_05.pdf'))
+intrvw_4 <- pdftools::pdf_text(here::here('data','Pioneer Mill Interviews', 'pioneermill_06.pdf'))
+intrvw_5 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_07.pdf'))
+intrvw_6 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_08.pdf'))
+intrvw_7 <- pdftools::pdf_text(here::here('data','Pioneer Mill Interviews', 'pioneermill_09.pdf'))
+intrvw_8 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_10.pdf'))
+intrvw_9 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_11.pdf'))
+intrvw_10 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_12.pdf'))
+intrvw_11 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_13.pdf'))
+intrvw_12 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_14.pdf'))
+intrvw_13 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_15.pdf'))
+intrvw_14 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_16.pdf'))
+intrvw_15 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_17.pdf'))
+intrvw_16 <- pdftools::pdf_text(here::here('data', 'Pioneer Mill Interviews','pioneermill_18.pdf'))
+
+
+lines_1 <- data.frame(intrvw_1) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_1, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_2 <- data.frame(intrvw_2) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_2, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+
+lines_3 <- data.frame(intrvw_3) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_3, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_4 <- data.frame(intrvw_4) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_4, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_5 <- data.frame(intrvw_5) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_5, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_6 <- data.frame(intrvw_6) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_6, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_7 <- data.frame(intrvw_7) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_7, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_8 <- data.frame(intrvw_8) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_8, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_9 <- data.frame(intrvw_9) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_9, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_10 <- data.frame(intrvw_10) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_10, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_11 <- data.frame(intrvw_11) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_11, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_12 <- data.frame(intrvw_12) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_12, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_13 <- data.frame(intrvw_13) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_13, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_14 <- data.frame(intrvw_14) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_14, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_15 <- data.frame(intrvw_15) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_15, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+lines_16 <- data.frame(intrvw_16) %>%
+  mutate(page = 1:n()) %>%
+  mutate(text_full = str_split(intrvw_16, pattern = '\\n')) %>%
+  unnest(text_full) %>% ## turns lines on the page and "pivot longer"
+  mutate(text_full = str_trim(text_full)) ## gets ride of white space
+
+
+words_1 <- lines_1 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_1)
+
+words_2 <- lines_2 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_2)
+
+words_3 <- lines_3 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_3)
+
+words_4 <- lines_4 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_4)
+
+words_5 <- lines_5 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_5)
+
+words_6 <- lines_6 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_6)
+
+words_7 <- lines_7 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_7)
+
+words_8 <- lines_8 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_8)
+
+words_9 <- lines_9 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_9)
+
+words_10 <- lines_10 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_10)
+
+words_11 <- lines_11 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_11)
+
+words_12 <- lines_12 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_12)
+
+words_13 <- lines_13 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_13)
+
+words_14 <- lines_14 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_14)
+
+words_15 <- lines_15 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_15)
+
+words_16 <- lines_16 %>%
+  unnest_tokens(word, text_full) %>%
+  select(-intrvw_16)
+
+wordcount_1 <- words_1 %>%
+  count(word)
+
+wordcount_2 <- words_2 %>%
+  count(word)
+
+wordcount_3 <- words_3 %>%
+  count(word)
+
+wordcount_4 <- words_4 %>%
+  count(word)
+
+wordcount_5 <- words_5 %>%
+  count(word)
+
+wordcount_6 <- words_6 %>%
+  count(word)
+
+wordcount_7 <- words_7 %>%
+  count(word)
+
+wordcount_8 <- words_8 %>%
+  count(word)
+
+wordcount_9 <- words_9 %>%
+  count(word)
+
+wordcount_10 <- words_10 %>%
+  count(word)
+
+wordcount_11 <- words_11 %>%
+  count(word)
+
+wordcount_12 <- words_12 %>%
+  count(word)
+
+wordcount_13 <- words_13 %>%
+  count(word)
+
+wordcount_14 <- words_14 %>%
+  count(word)
+
+wordcount_15 <- words_15 %>%
+  count(word)
+
+wordcount_16 <- words_16 %>%
+  count(word)
+
+
+wc_clean_1 <- wordcount_1 %>%
+  anti_join(stop_words, by = 'word') %>%
+  filter(word != 'hy') %>%
+  filter(word != 'jk')%>%
+  filter(word != 'yeah')
+
+
+wc_clean_2 <- wordcount_2 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'wn') %>%
+  filter(word != 'ss') %>%
+  filter(word != 'yeah')
+
+wc_clean_3 <- wordcount_3 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'wn') %>%
+  filter(word != 'mh') %>%
+  filter(word != 'yeah')
+
+wc_clean_4 <- wordcount_4 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'wn') %>%
+  filter(word != 'dr') %>%
+  filter(word != 'yeah')
+
+
+wc_clean_5 <- wordcount_5 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'wn') %>%
+  filter(word != 'sh') %>%
+  filter(word != 'yeah') %>%
+  filter(word != 'em') %>%
+  filter(word != 'mh')
+
+
+
+wc_clean_6 <- wordcount_6 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'wn') %>%
+  filter(word != 'av') %>%
+  filter(word != 'yeah')
+
+wc_clean_7 <- wordcount_7 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'em') %>%
+  filter(word != 'wn') %>%
+  filter(word != 'yeah')  %>%
+  filter(word != 'bb') %>%
+  filter(word != 'eh')
+
+
+wc_clean_8 <- wordcount_8 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'mo') %>%
+  filter(word != 'jh') %>%
+  filter(word != 'yeah') %>%
+  filter(word != 'em')
+
+
+wc_clean_9 <- wordcount_9 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'em') %>%
+  filter(word != 'mo') %>%
+  filter(word != 'yeah')  %>%
+  filter(word != 'fh') %>%
+  filter(word != 'pu') %>%
+  filter(word != 'ukoli')
+
+
+wc_clean_10 <- wordcount_10 %>%
+  anti_join(stop_words, by = 'word') %>%
+  filter(word != 'mo') %>%
+  filter(word != 'sk') %>%
+  filter(word != 'yeah') %>%
+  filter(word != '')
+
+wc_clean_11 <- wordcount_11 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'wn') %>%
+  filter(word != 'hf') %>%
+  filter(word != 'yeah') %>%
+  filter(word != 'em')
+
+wc_clean_12 <- wordcount_12 %>%
+  anti_join(stop_words, by = 'word') %>%
+  filter(word != 'wn') %>%
+  filter(word != 'df') %>%
+  filter(word != 'yeah')
+
+
+wc_clean_13 <- wordcount_13 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'ak') %>%
+  filter(word != 'alk') %>%
+  filter(word != 'yeah')  %>%
+  filter(word != 'tk') %>%
+  filter(word != 'wn')
+
+
+
+wc_clean_14 <- wordcount_14 %>%
+  anti_join(stop_words, by = 'word')  %>%
+  filter(word != 'mh') %>%
+  filter(word != 'hy') %>%
+  filter(word != 'yeah')
+
+wc_clean_15 <- wordcount_15 %>%
+  anti_join(stop_words, by = 'word')
+
+wc_clean_16 <- wordcount_16 %>%
+  anti_join(stop_words, by = 'word') %>%
+  filter(word != 'mo') %>%
+  filter(word != 'td') %>%
+  filter(word != 'em') %>%
+  filter(word != 'yeah')
+
+top_1 <- wc_clean_1 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 1)
+
+top_2 <- wc_clean_2 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 2)
+
+top_3 <- wc_clean_3 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 3)
+
+top_4 <- wc_clean_4 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 4)
+
+top_5 <- wc_clean_5 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 5)
+
+
+top_6 <- wc_clean_6 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 6)
+
+top_7 <- wc_clean_7 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 7)
+
+top_8 <- wc_clean_8 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 8)
+
+top_9 <- wc_clean_9 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 9)
+
+top_10 <- wc_clean_10 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 10)
+
+top_11 <- wc_clean_11 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 11)
+
+top_12 <- wc_clean_12 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 12)
+
+top_13 <- wc_clean_13 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 13)
+
+top_14 <- wc_clean_14 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 14)
+
+top_15 <- wc_clean_15 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 15)
+
+top_16 <- wc_clean_16 %>%
+  arrange(-n) %>%
+  slice(1:10) %>%
+  mutate(interview = 16)
+
+
+unions <- rbind(top_1,top_2,top_3,top_4,top_5,top_6,top_7,top_8,top_9,top_10,top_11,top_12,top_13,top_14,top_15,top_16)
+
+# Make some graphs:
+ggplot(data = unions, aes(x = n, y = word)) +
+  geom_col(fill = "blue") +
+  facet_wrap(~interview, scales = "free")
+
